20e0ac9d61bc45239be970caad970a0f36e6484c
William Jones <wjones19@umd.edu>
2022-04-07 15:01:57 -0400

    ranked score function

---
+++ rank_score.py
@@ -0,0 +1,21 @@
+# list comprehension
+
+def ranked_scores(score_file):
+	"""Sort the high score file and returns top 5 overall scores.
+
+	Args:
+		score_file (str): File path for the high score file.
+
+	Side effects:
+		Prints information to the terminal
+	"""
+	high_score_dict = {}
+	with open(score_file, 'r', encoding='utf-8') as f:
+		for line in f:
+			line.split(':')
+			high_score_dict[line[0]]=line[1]
+		ranked_dict=(sorted(high_score_dict, key=high_score_dict.get, 
+			reverse=True)[:5])
+		print(ranked_dict)
+
+ranked_scores("scores.txt")
