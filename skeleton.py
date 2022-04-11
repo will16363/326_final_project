5e0ee246152e14953de2b856e6233771027905a4
William Jones <wjones19@umd.edu>
2022-04-11 08:44:51 -0400

    started skeleton file

--- skeleton.py
+++ skeleton.py
@@ -1,1 +1,29 @@
-# Enter your skeleton stuff here
+# if __name__ == "__main__":
+
+""" Score (f-strings) """
+"""def score(self, player_score, attack_results):
+	Keeps track of the player's score upon completion of the round.
+
+	Args:
+		player_score (int): The player's score before the round starts
+		attack_results (bool): The result of the player attack method
+	
+	Side effects:
+		Prints information to the terminal during a round of Zombie Rolls
+	
+	Returns:
+		new_score (int): The player's score after the completion of the round
+"""
+
+""" High Score (with statements) """
+"""def high_score(player_name, new_score, score_file):
+	Writes the player's score to a high score file.
+
+	Args:
+		new_score (int): Score of the player at the end of the game.
+		player_name (str): Name of the player (maybe use self.name?)
+		score_file (str): File path for the high score file.
+
+	Side effects:
+		Writes to a file that high scores are kept in.
+"""
