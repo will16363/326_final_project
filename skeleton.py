fcb11afb97eef772d5a5f7241987653f13e2af70
William Jones <wjones19@umd.edu>
2022-04-12 14:27:21 -0400

    Changes made by Will

--- skeleton.py
+++ skeleton.py
@@ -3,8 +3,8 @@
 """ Score (f-strings) """
 """def score(self, player_score, attack_results):
 	Keeps track of the player's score upon completion of the round. This 
-	function will use the results of the attack() method which returns 
-	True or False. This function will have variables containing points for 
+	method will use the results of the attack() method which returns 
+	True or False. This method will have variables containing points for 
 	beating the Zombie or losing to the Zombie. This will then add the 
 	corresponding amount of points to the player's score which is being passed
 	in from the previous round.
@@ -23,7 +23,7 @@
 
 """ High Score (with statements) """
 """def high_score(player_name, new_score, score_file):
-	Writes the player's score to a high score file. This function takes the 
+	Writes the player's score to a high score file. This method takes the 
 	player's name from the game class, the player's score from the score() 
 	method, and a file name. It then appends the player's name and score to the
 	file.
@@ -40,7 +40,7 @@
 
 """ Ranked Score (custom list sorting using lambda) """
 """def ranked_scores(score_file):
-	Sort the high score file and returns top 5 overall scores. This function 
+	Sort the high score file and returns top 5 overall scores. This method 
 	takes the high score file and creates an empty dictionary. It then opens the
 	score file and splits each line so that the player's name and score are 
 	added to the dictionary. A lambda function is then used to sort and print 
@@ -56,7 +56,7 @@
 
 """ Music """
 """ def play_music(path):
-	Plays music during the game. This function opens an mp3 file and uses both 
+	Plays music during the game. This method opens an mp3 file and uses both 
 	the playsound and glob modules to play the music during a game of Zombie
 	Rolls.
 	
@@ -68,12 +68,12 @@
 """
 
 
-""" Round Function (optional parameters) """
+""" Round Method (optional parameters) """
 """ def round_fct(round_number, skip_supply="False"):
-	Keeps track of the round within a game of Zombie Rolls. This function takes
-	the round number from the main() function which has an initial value of 0. 
-	This function then adds 1 to the round number and returns it so that the 
-	round can be tracked. This function also prints messages to the terminal 
+	Keeps track of the round within a game of Zombie Rolls. This method takes
+	the round number from the main() method which has an initial value of 0. 
+	This method then adds 1 to the round number and returns it so that the 
+	round can be tracked. This method also prints messages to the terminal 
 	for the player. The player has the ability to skip the supply runs, which 
 	are even rounds from 0-13. If the player skips the supply run, they go 
 	straight to the Zombie fight and the message that is played reflects that.
@@ -89,4 +89,20 @@
 	
 	Returns:
 		round_num (int): the number correspoding to the round in the game
+"""
+
+""" Game Over Method """
+"""def game_over(health, round_num):
+    Determines when the game of Zombie Rolls is officially over. This method 
+	will take the health from the Health() method and the round number from the 
+	Round() method. The game is considered over if the player has no health left 
+	or has defeated the final zombie at round 13. 
+    
+    Args:
+        health (int): The health of the player expressed as an integer
+        round_num (int): The round number expressed as an integer
+
+    Returns:
+        game_status (bool): The status of the game, True if the game is over, 
+            otherwise False.
 """
