24a92bef15cf045279cc97eb5d9281d5d9e24401
William Jones <wjones19@umd.edu>
2022-05-15 12:10:40 -0400

    sdfds

--- main.py
+++ main.py
@@ -94,25 +94,6 @@ def print_status_bar(ZombiePlayer): #needs to represent two of the things from l
         print("0% [          ]")
 
 
-# needs doctrings
-def decrease_health(Zombie_Player, damage):
-    if Zombie_Player.player_health - damage <= 0:
-        game_over()
-    else:
-        Zombie_Player.player.health -= damage
-    if Zombie_Player.zombie_health - damage <= 0:
-        Zombie_Player.zombie_health = 0
-        
-
-# needs doctrings
-def increase_health(ZombiePlayer, heal):
-    if ZombiePlayer.player_health + heal >= 100:
-        ZombiePlayer.player_health = 100
-    else:
-        ZombiePlayer.player_health += heal
-#^^^^put these two in the zombie class so you can ZombiePlayer.decrease_health(pass in dmag or healing)
-
-
 def game_over(ZombiePlayer, round_num):                
     """Determines when the game of Zombie Rolls is officially over. The game is 
        considered over if the player has no health left or has defeated the 
