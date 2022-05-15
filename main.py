0dbe2bfaebc8f14ae653991e3ad2d84557b8ddd3
Takuya Kameyama <tictaks52@gmail.com>
2022-05-15 12:33:39 -0400

    update super

--- main.py
+++ main.py
@@ -243,8 +243,8 @@ class ZombiePlayer:
     """    
     def __init__(self, player, zombie, chosen_weapon):   
         """Initialize new zombie and player object."""
-        self.zombie = player      
-        self.player = zombie      
+        self.zombie = zombie      
+        self.player = player      
         self.player_health = 100
         self.zombie_health = 25
         self.damage = 0
@@ -264,13 +264,13 @@ class ZombiePlayer:
             if zombie_roll > player_roll:
                 self.damage = int(zombie_roll) - int(player_roll)              
                 print(f'{self.player} took {self.damage} damage.')
-                decrease_health(self.player, self.damage)
+                self.decrease_health(self.player, self.damage)
                 print_status_bar(self)
                 input("Press Enter to continue...")
             elif zombie_roll <= player_roll:
                 self.damage = int(self.weapon) #the damage of the weapon that the player chooses to use           
                 print(f'{self.zombie} took {self.damage} damage.')
-                decrease_health(self.zombie, self.damage)
+                self.decrease_health(self.zombie, self.damage)
                 print_status_bar(self)
                 input("Press Enter to continue...")
         if self.zombie.health <= 0:
@@ -350,6 +350,7 @@ class BossZombie(ZombiePlayer):
         print_status_bar(self)
         input("Press Enter to continue...")
         super().attack()
+        super().decrease_health()
 
 
 def main(): #do this in chronologicl order how the game will play out 
